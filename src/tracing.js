4bcff2ecba5c692229fb805a8dbe5c2cbc90ace6
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2022-05-12 16:24:28 -0700

    chore: optionally enable JaegerExporter

--- src/tracing.js
+++ src/tracing.js
@@ -9,15 +9,16 @@ const provider = new BasicTracerProvider({
   instrumentations: [new AwsInstrumentation({})],
 });
 
-// Configure span processor to send spans to the exporter
+// Configure span processor to send spans to the ConsoleSpanExporter
 provider.addSpanProcessor(new SimpleSpanProcessor(new ConsoleSpanExporter()));
 
-if (process.env.OTEL_EXPORTER_JAEGER_ENABLED === "true") {
-// Configure span processor to send spans to the exporter
-const exporter = new JaegerExporter({
-  endpoint: 'http://localhost:14268/api/traces',
-});
-provider.addSpanProcessor(new SimpleSpanProcessor(exporter));
+// Configure span processor to send spans to the JaegerExporter
+if (process.env.OTEL_EXPORTER_JAEGER_ENABLED) {
+  const { JaegerExporter } = await import("@opentelemetry/exporter-jaeger");
+  const exporter = new JaegerExporter({
+    endpoint: "http://localhost:14268/api/traces",
+  });
+  provider.addSpanProcessor(new SimpleSpanProcessor(exporter));
 }
 
 provider.register();
