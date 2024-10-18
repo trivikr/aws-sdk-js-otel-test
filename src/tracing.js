a75287df9b730e3f65ac9d2d4ddb4abf04c0ed3a
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2024-10-18 09:40:04 -0700

    Re-introduce .editorconfig and use it in biome

--- src/tracing.js
+++ src/tracing.js
@@ -1,15 +1,15 @@
 import { AwsInstrumentation } from "@opentelemetry/instrumentation-aws-sdk";
 import { Resource } from "@opentelemetry/resources";
 import {
-	ConsoleSpanExporter,
-	SimpleSpanProcessor,
+  ConsoleSpanExporter,
+  SimpleSpanProcessor,
 } from "@opentelemetry/sdk-trace-base";
 import { NodeTracerProvider } from "@opentelemetry/sdk-trace-node";
 import { ATTR_SERVICE_NAME } from "@opentelemetry/semantic-conventions";
 
 const provider = new NodeTracerProvider({
-	resource: new Resource({ [ATTR_SERVICE_NAME]: "test-aws-sdk" }),
-	instrumentations: [new AwsInstrumentation({})],
+  resource: new Resource({ [ATTR_SERVICE_NAME]: "test-aws-sdk" }),
+  instrumentations: [new AwsInstrumentation({})],
 });
 
 // Configure span processor to send spans to the ConsoleSpanExporter
@@ -17,11 +17,11 @@ provider.addSpanProcessor(new SimpleSpanProcessor(new ConsoleSpanExporter()));
 
 // Configure span processor to send spans to the JaegerExporter
 if (process.env.JAEGER_ENABLED) {
-	const { JaegerExporter } = await import("@opentelemetry/exporter-jaeger");
-	const exporter = new JaegerExporter({
-		endpoint: "http://localhost:14268/api/traces",
-	});
-	provider.addSpanProcessor(new SimpleSpanProcessor(exporter));
+  const { JaegerExporter } = await import("@opentelemetry/exporter-jaeger");
+  const exporter = new JaegerExporter({
+    endpoint: "http://localhost:14268/api/traces",
+  });
+  provider.addSpanProcessor(new SimpleSpanProcessor(exporter));
 }
 
 provider.register();
